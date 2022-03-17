49b7c4063480fab3d0bdcf614277880aca5dc6ee
bin <tuduweb@qq.com>
2022-03-17 19:47:49 +0800

    增加win(x64)下的mvcamsdk库及配置。

--- mvcamsdk/mvcamsdk.cmake
+++ mvcamsdk/mvcamsdk.cmake
@@ -1,3 +1,6 @@
+
+add_library(mvcam_lib STATIC)
+
 IF(APPLE)
 #${CMAKE_SOURCE_DIR}/mvcamsdk
 set(mvcamsdk_libs_DIR ${CMAKE_SOURCE_DIR}/mvcamsdk/darwin/lib)
@@ -17,14 +20,34 @@ message("HEADERS = ${HEADERS}")
 include_directories( ${mvcamsdk_headers_DIR} )
 link_directories(${mvcamsdk_libs_DIR} )
 
+target_link_libraries(mvcam_lib
+    PUBLIC libmvsdk
+)
 
 ELSEIF(WIN32)
+
+set(mvcamsdk_libs_DIR ${CMAKE_SOURCE_DIR}/mvcamsdk/win32/lib)
+set(mvcamsdk_headers_DIR ${CMAKE_SOURCE_DIR}/mvcamsdk/win32/include)
+
+add_library(libmvsdk STATIC IMPORTED)
+#指定所添加依赖库的导入路径 #为libmvsdk 指定属性 IMPORTED_LOCATION ..
+set_target_properties(libmvsdk PROPERTIES IMPORTED_LOCATION ${mvcamsdk_libs_DIR}/MVCAMSDK_X64.lib )
+
+# file(GLOB LIBRARIES ${mvcamsdk_libs_DIR}/*_X64.lib)
+# message("LIBRARIES = ${LIBRARIES}")
+
+file(GLOB HEADERS ${mvcamsdk_headers_DIR}/*)
+message("HEADERS = ${HEADERS}")
+
+include_directories( ${mvcamsdk_headers_DIR} )
+#link_directories(${mvcamsdk_libs_DIR} )
+
+
+target_link_libraries(mvcam_lib
+    #PUBLIC ${mvcamsdk_libs_DIR}/MVCAMSDK_X64.lib
+    PUBLIC libmvsdk
+)
 
 ELSE()
 
 ENDIF()
-
-add_library(mvcam_lib STATIC)
-target_link_libraries(mvcam_lib
-    PUBLIC libmvsdk
-)
